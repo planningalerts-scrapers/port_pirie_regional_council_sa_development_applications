dc7220c92bc04367d138a315f6639be30e075a45
MichaelBone <Michael.Bone@senet.com.au>
2018-08-03 22:42:23 +0930

    Initial implementation.

--- scraper.js
+++ scraper.js
@@ -1,59 +1,76 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
+// Parses the development application at the South Australian Port Pirie Regional Council site
+// and places them in a database.
+//
+// Michael Bone
+// 3rd August 2018
 
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+"use strict";
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+let cheerio = require("cheerio");
+let request = require("request-promise-native");
+let sqlite3 = require("sqlite3").verbose();
+let pdf2json = require("pdf2json");
+let urlparser = require("url");
+let moment = require("moment");
+
+const DevelopmentApplicationsUrl = "http://www.pirie.sa.gov.au/page.aspx?u=646#.W2REvfZuKUl";
+const CommentUrl = "mailto:council@pirie.sa.gov.au";
+
+// Sets up an sqlite database.
+
+async function initializeDatabase() {
+    return new Promise((resolve, reject) => {
+        let database = new sqlite3.Database("data.sqlite");
+        database.serialize(() => {
+            database.run("create table if not exists [data] ([council_reference] text primary key, [address] text, [description] text, [info_url] text, [comment_url] text, [date_scraped] text, [date_received] text, [on_notice_from] text, [on_notice_to] text)");
+            resolve(database);
+        });
+    });
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
+// Inserts a row in the database if it does not already exist.
+
+async function insertRow(database, developmentApplication) {
+    return new Promise((resolve, reject) => {
+        let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        sqlStatement.run([
+            developmentApplication.applicationNumber,
+            developmentApplication.address,
+            developmentApplication.reason,
+            developmentApplication.informationUrl,
+            developmentApplication.commentUrl,
+            developmentApplication.scrapeDate,
+            developmentApplication.receivedDate,
+            null,
+            null
+        ], function(error, row) {
+            if (error) {
+                console.error(error);
+                reject(error);
+            } else {
+                if (this.changes > 0)
+                    console.log(`    Inserted: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" into the database.`);
+                else
+                    console.log(`    Skipped: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and reason \"${developmentApplication.reason}\" because it was already present in the database.`);
+                sqlStatement.finalize();  // releases any locks
+                resolve(row);
+            }
+        });
+    });
 }
 
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
+// Parses the development applications.
+
+async function main() {
+    // Ensure that the database exists.
+
+    let database = await initializeDatabase();
+
+    // Retrieve the page contains the links to the PDFs.
+
+    console.log(`Retrieving page: ${DevelopmentApplicationsUrl}`);
+    let body = await request(DevelopmentApplicationsUrl);
+    let $ = cheerio.load(body);
 }
 
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
-
-		callback(body);
-	});
-}
-
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
-		readRows(db);
-
-		db.close();
-	});
-}
-
-initDatabase(run);
+main().then(() => console.log("Complete.")).catch(error => console.error(error));
